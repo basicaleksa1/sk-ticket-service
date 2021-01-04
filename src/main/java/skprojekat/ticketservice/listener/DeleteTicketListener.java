05ddbb7b0f38f7f40c275f70fa3aac3d5a9b9418
Aleksa <basicaleksa1@gmail.com>
2021-01-05 00:21:01 +0100

    popravka

--- src/main/java/skprojekat/ticketservice/listener/DeleteTicketListener.java
+++ src/main/java/skprojekat/ticketservice/listener/DeleteTicketListener.java
@@ -7,14 +7,34 @@ import javax.jms.TextMessage;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Component
 public class DeleteTicketListener {
+	
+	private ObjectMapper objectMapper;
+	
+	public DeleteTicketListener(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
 
 	@JmsListener(destination="${destination.delete_flight}", concurrency = "5-10")
 	public void handleDeleteTicket(Message message) {
 		try {
 			String jsonText = ((TextMessage)message).getText();
-			System.out.println("alalh");
+			try {
+				Integer id = objectMapper.readValue(jsonText, Integer.class);
+				System.out.println(id);
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
